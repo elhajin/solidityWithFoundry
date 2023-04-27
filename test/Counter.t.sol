f0e87bdea6216b7f1944eb2539d1f8bb2ce532a2
elhajin <30.06.elhajin@gmail.com>
2023-04-27 18:56:19 +0300

    save

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,24 +1,48 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.17;
 
 import "forge-std/Test.sol";
-import "../src/Counter.sol";
+import "../src/counter.sol";
 
 contract CounterTest is Test {
     Counter public counter;
+    address ownBy;
 
+    //setUp should be public or external:
     function setUp() public {
         counter = new Counter();
-        counter.setNumber(0);
+        console.log("the contract is deployed at ", address(counter));
     }
 
-    function testIncrement() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+    function test_sum(uint x) public {
+        counter.sum(x);
+        uint newVal = counter.result();
+        assertEq(newVal, x);
     }
 
-    function testSetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    function testFailDec() public {
+        counter.dec(1);
+    }
+
+    function test_decWithError() public {
+        vm.expectRevert(stdError.arithmeticError); //this refer to type of error
+        counter.dec(2);
+    }
+
+    function test_changeOwner() public {
+        assertEq(counter.owner(), address(this));
+        counter.changeOwner(address(counter));
+        address newOwner = counter.owner();
+        assertEq(newOwner, address(counter));
+    }
+
+    function test_ChangeOwnerFail() public {
+        vm.prank(address(120));
+        vm.expectRevert("modifier fail check -NOT OWNER-"); //it'takes message error
+        counter.changeOwner(address(counter));
     }
 }
+/* TEST KEYWORDS:
+- assertNotEq(value1, value2) : that the provided values not equal;
+-
+*/
